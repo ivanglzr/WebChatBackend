720b2c56c2199ae7582ff203725cdb339a243aae
Ivan <ivanglzr01@gmail.com>
2025-01-05 17:27:41 +0100

    chore: initial handle socket functionality

--- src/io.ts
+++ src/io.ts
@@ -1,9 +1,48 @@
+import prisma from "./prisma";
+
+import { tokenService } from "./auth/services";
+
 import type { Server } from "socket.io";
+import { UUID } from "crypto";
 
-export default function handleSocket(io: Server) {
-  io.on("connection", (socket) => {
-    console.log("a user connected");
+export class Io {
+  public static io: Server;
+}
 
-    socket.on("message", (msg) => io.emit("message", msg));
+export function handleSocket(io: Server) {
+  Io.io = io;
+
+  Io.io.use(async (socket, next) => {
+    if (!socket.handshake.auth.token) {
+      socket.disconnect();
+
+      return;
+    }
+
+    const payload = await tokenService.verify(socket.handshake.auth.token);
+
+    if (!payload || !payload.id) {
+      socket.disconnect();
+
+      return;
+    }
+
+    socket.handshake.auth.id = payload.id;
+  });
+
+  Io.io.on("connection", async (socket) => {
+    const id = socket.handshake.auth.id as UUID;
+
+    socket.join(`User ${id}`);
+
+    const chats = await prisma.chats.findMany({
+      where: { usersIds: { has: id } },
+    });
+
+    chats.map((chat) => {
+      socket.join(`Chat ${chat.id}`);
+    });
+
+    socket.on("disconnect", () => console.log("user disconnected"));
   });
 }
