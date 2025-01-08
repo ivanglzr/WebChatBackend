ac19cc139f090888a80e40329befda6b6ae5d9d3
Ivan <ivanglzr01@gmail.com>
2025-01-08 21:43:32 +0100

    fix. add a middleware for id validation

--- src/message/router.ts
+++ src/message/router.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
 import { messageController } from "./controller";
+import validateId from "@/common/middlewares/validateId";
 
 const messageRouter = Router({ mergeParams: true });
 
 messageRouter.get("/", messageController.getMessages);
-messageRouter.get(":messageId", messageController.getMessage);
+messageRouter.get(
+  "/:messageId",
+  validateId("messageId"),
+  messageController.getMessage
+);
 
 export default messageRouter;
